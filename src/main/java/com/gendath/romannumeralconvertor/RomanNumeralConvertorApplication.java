316b2d728ebe20eaafa20dc204adbac2d65ca90c
gendath <sellwithperry@gmail.com>
2022-09-22 18:50:29 -0500

    init

--- src/main/java/com/gendath/romannumeralconvertor/RomanNumeralConvertorApplication.java
+++ src/main/java/com/gendath/romannumeralconvertor/RomanNumeralConvertorApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RomanNumeralConvertorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RomanNumeralConvertorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RomanNumeralConvertorApplication.class, args);
+
+    }
 
 }
