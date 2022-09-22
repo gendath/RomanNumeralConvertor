3cc54315f07ab81a69355f5e6c1a8e7d467c1a2f
gendath <sellwithperry@gmail.com>
2022-09-22 17:51:08 -0500

    Initial commit

---
+++ src/main/java/com/gendath/romannumeralconvertor/RomanNumeralConvertorApplication.java
@@ -0,0 +1,13 @@
+package com.gendath.romannumeralconvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanNumeralConvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomanNumeralConvertorApplication.class, args);
+	}
+
+}
