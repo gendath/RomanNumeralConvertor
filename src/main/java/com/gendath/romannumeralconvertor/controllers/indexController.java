b84a392fccb6e0b2d5ce24bec82d3b8134d38b66
gendath <sellwithperry@gmail.com>
2022-09-22 21:23:43 -0500

    implement controller

---
+++ src/main/java/com/gendath/romannumeralconvertor/controllers/indexController.java
@@ -0,0 +1,37 @@
+package com.gendath.romannumeralconvertor.controllers;
+
+import com.gendath.romannumeralconvertor.services.RomanNumeralConvertorService;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class indexController {
+
+    RomanNumeralConvertorService service;
+
+    public indexController(RomanNumeralConvertorService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+        return "index";
+    }
+
+
+    @PostMapping(value = "/convert", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String convert(int number, Model model){
+
+
+            model.addAttribute("result", service.toRoman(number));
+
+
+
+        return "index";
+
+
+    }
+}
